269efbea69b0de4b5b4e1eb9c6eb81569e6f0dec
Md.Nourose Naim Rhedoy <md.nourosenaimrhedoy@MdNouroses-MacBook-Pro.local>
2024-02-27 00:37:57 +0600

    form design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TestComponent from './components/test-component/test-component';
+
 import AuthComponent from './components/auth-component/auth-component';
+import TestComponent from './components/test-component/test-component';
 
 function App() {
   return (
     <div className="App">
-      {/* <TestComponent name="Shojib" age={50}/> */}
+     
       <AuthComponent/>
     </div>
   );
--- src/components/auth-component/auth-component.tsx
+++ src/components/auth-component/auth-component.tsx
@@ -1,11 +1,68 @@
-import React from "react";
-import './auth-component.css'
+import React, { useState, ChangeEvent } from "react";
+import './auth-component.css';
 
+const AuthComponent: React.FC = () => {
+    const [imagePreview, setImagePreview] = useState<string | null>(null);
 
-const AuthComponent = ()=>{
-    return(
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            if (typeof reader.result === "string") {
+                setImagePreview(reader.result);
+            }
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    };
+
+    return (
         <div className="container">
-            This is Auth
+            <form action="">
+                
+                <label htmlFor="fname">First Name:</label><br />
+                <input type="text" id='fname' name="fname" /><br />
+                <label htmlFor="lname">Last Name:</label><br />
+                <input type="text" id='lname' name='lname' /><br />
+                <label htmlFor="age">Age:</label><br />
+                <input type="text" id="age" name="age" /><br />
+                <label htmlFor="email">Email:</label><br />
+                <input type="email" id="email" name="email" /><br />
+                <label htmlFor="password">Password:</label><br />
+                <input type="password" id="password" name="password" /><br />
+                <label htmlFor="bio">Bio:</label><br />
+                <textarea id="bio" name="bio" /><br />
+                <label htmlFor="gender">Gender:</label><br />
+                <select id="gender" name="gender">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                </select><br />
+                <label htmlFor="avatar">Avatar:</label><br />
+                <input type="file" id="avatar" name="avatar" accept="image/*" onChange={handleFileChange} /><br />
+                {imagePreview && (
+                    <img src={imagePreview} alt="Avatar Preview" style={{ maxWidth: '100px', maxHeight: '100px' }} />
+                )}
+                <br />
+                <label htmlFor="agree">Agree to terms:</label>
+                <input type="checkbox" id="agree" name="agree" />
+                <br />
+                <label htmlFor="subscribe">Subscribe to newsletter:</label>
+                <input type="checkbox" id="subscribe" name="subscribe" />
+                <br />
+                <label>Choose your role:</label>
+                <br />
+                <input type="radio" id="student" name="role" value="student" />
+                <label htmlFor="student">Student</label>
+                <br />
+                <input type="radio" id="teacher" name="role" value="teacher" />
+                <label htmlFor="teacher">Teacher</label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
